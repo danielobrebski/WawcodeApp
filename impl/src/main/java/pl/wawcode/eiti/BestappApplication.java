f5687379ba2f8ec890ce4ed74596ad919968b8a8
danielobrebski <dani@notebookcheck.pl>
2017-10-10 22:58:41 +0200

    First commit

---
+++ impl/src/main/java/pl/wawcode/eiti/BestappApplication.java
@@ -0,0 +1,12 @@
+package pl.wawcode.eiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestappApplication.class, args);
+	}
+}
