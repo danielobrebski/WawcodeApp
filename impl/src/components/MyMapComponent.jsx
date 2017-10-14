5cbc5369274e816778ae3baf69d06578a25a7f24
xarines <wmarch112@gmail.com>
2017-10-14 20:24:04 +0200

    add timeout to getShitters method in frontend

--- impl/src/components/MyMapComponent.jsx
+++ impl/src/components/MyMapComponent.jsx
@@ -5,13 +5,35 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 const MyMapComponent = withScriptjs(withGoogleMap(
   class extends React.Component {
 
+
+
+    constructor(props) {
+        super(props);
+        this.trottledOnChangedBound = this.trottledOnChangedBound.bind(this);
+        this.lastApiCall = 0;
+        this.timeout = null;
+    }
+
+    trottledOnChangedBound() {
+        const TIMEOUT = 500;
+        let now = Date.now();
+        if (now - this.lastApiCall > TIMEOUT) {
+            this.lastApiCall = now;
+            this.ref && this.props.onChangedBounds(this.ref.getBounds());
+        } else {
+            clearTimeout(this.timeout);
+            this.timeout = setTimeout(() => this.ref && this.props.onChangedBounds(this.ref.getBounds()),
+                now - this.lastApiCall)
+        }
+    }
+
     render() {
       return (
         <GoogleMap
           ref={ref => this.ref = ref}
           defaultZoom={8}
           defaultCenter= {this.props.userLocation}
-          onBoundsChanged = {e => this.ref && this.props.onChangedBounds(this.ref.getBounds())}>
+          onBoundsChanged = {this.trottledOnChangedBound}>
           {this.props.data && this.props.data.map(({latitude,longitude}, key) =>
             <Marker position={ {lat : latitude , lng : longitude} }
                     key = {key} />)}
